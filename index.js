eaf6ad836c14a2a3d7037201301371badf37708d
shm_usuf <shmusuf@gmail.com>
2023-03-06 18:58:35 +0600

    project server initialized

---
+++ index.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+//express app
+const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+//port
+const PORT = process.env.PORT || 4000;
+
+//connect to db
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    //listening for requests
+    app.listen(PORT, (res, req) => {
+      console.log(`connected to db and server is running on port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
